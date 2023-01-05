85a53a5140c4965864736210a179cf1ef824caeb
Jannik Frei <jannik.frei@students.fhnw.ch>
2023-01-05 16:44:08 +0100

    add map migrate

---
+++ db/migrate/20230105134551_create_maps.rb
@@ -0,0 +1,12 @@
+class CreateMaps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :maps do |t|
+      t.string :title
+      t.decimal :lat
+      t.decimal :lng
+      t.decimal :zoom
+
+      t.timestamps
+    end
+  end
+end
