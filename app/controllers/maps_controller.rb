dfda506e6c7dbb09c188f0fdb132e335dfbf2cc4
Jannik Frei <jannik.frei@students.fhnw.ch>
2023-01-05 16:39:38 +0100

    functional map crud

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,49 @@
 class MapsController < ApplicationController
+  before_action :set_map, only: [:show, :edit, :update, :destroy]
+
   def index
-    @model_data = Map.all
+    @maps = Map.all
+  end
+
+  def show
+  end
+
+  def new
+    @map = Map.new
+  end
+
+  def edit
+  end
+
+  def create
+    @map = Map.new(map_params)
+    if @map.save
+      redirect_to @map
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @map.update(map_params)
+      redirect_to @map, notice: 'Map was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @map = Map.find(params[:id])
+    @map.destroy
+    redirect_to maps_path
+  end
+
+  private
+  def set_map
+    @map = Map.find(params[:id])
+  end
+
+  def map_params
+    params.require(:map).permit(:title, :lat, :lng, :zoom)
   end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,2 +1,6 @@
 class Map < ApplicationRecord
+  validates :title, presence: true
+  validates :lat, presence: true
+  validates :lng, presence: true
+  validates :zoom, presence: true
 end
