abfdbe6a18452b4bf41c15fdc9e05f5e76934e4a
Jannik Frei <jannik.frei@students.fhnw.ch>
2023-01-13 10:30:05 +0100

    imageSet functionality

--- app/models/image_set.rb
+++ app/models/image_set.rb
@@ -2,4 +2,5 @@ class ImageSet < ApplicationRecord
   validates :typ, presence: true
   validates :color, presence: true
   belongs_to :map
+  has_many :images
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -4,4 +4,5 @@ class Map < ApplicationRecord
   validates :lng, presence: true
   validates :zoom, presence: true
   has_many :image_sets
+  has_many :overlays
 end
